93c42e2ea6905b55685ea4b054bc9dcbbef9b899
Stefan Niederhauser <ghuder5@gmx.ch>
2021-09-12 02:08:35 +0200

    add dummy classes to generate dummy javadocs

--- mbrola-jvm-linux/src/main/kotlin/guru/nidi/mbrola/linux/MbrolaJvmLinux.kt
+++ mbrola-jvm-linux/src/main/kotlin/guru/nidi/mbrola/linux/MbrolaJvmLinux.kt
@@ -13,6 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package guru.nidi.mbrola
+package guru.nidi.mbrola.linux
 
-object MbrolaJvmWindows
+object MbrolaJvmLinux
